2c24c7a87f3e4caead176f1514ccd6c53fb777b5
Adizmobile <adizmobile.com@gmail.com>
2018-05-16 09:43:37 +0700

    Java While

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+      int tong = 0;
+      int i = 0;
+      while (i< 10){
+          tong = tong + i;
+          i = i + 1;
+      }
+      System.out.println("Tổng Từ 1 Đến 9 Là : " + tong);
+
+    }
+}
